f3fa8a4711e161f98efe1ff6fe5e8a45cca81573
Justin Nauman <justin.r.nauman@gmail.com>
2020-03-27 07:30:41 -0500

    Bump on brewfiles

--- .bashrc.d/000.kubernetes.sh
+++ .bashrc.d/000.kubernetes.sh
@@ -3,3 +3,6 @@ alias unhealthy-pods='kubectl get pods -o wide --all-namespaces | grep -v Runnin
 alias watch-events='kubectl get events --all-namespaces -w --watch-only'
 alias kcc='kubectl config use-context'
 alias kubeevents='kubectl get events --all-namespaces --sort-by='.metadata.creationTimestamp''
+
+# Krew paths
+export PATH=$HOME/.krew/bin:$PATH
